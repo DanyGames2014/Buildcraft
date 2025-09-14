0fa9a98b71627037d71cf9ac3bf988fed77d2559
Daniel Fildán <fildand@centrum.cz>
2025-09-14 19:41:16 +0200

    Gears

--- src/main/java/net/danygames2014/buildcraft/Buildcraft.java
+++ src/main/java/net/danygames2014/buildcraft/Buildcraft.java
@@ -1,6 +1,10 @@
 package net.danygames2014.buildcraft;
 
+import net.mine_diver.unsafeevents.listener.EventListener;
+import net.minecraft.item.Item;
+import net.modificationstation.stationapi.api.event.registry.ItemRegistryEvent;
 import net.modificationstation.stationapi.api.mod.entrypoint.Entrypoint;
+import net.modificationstation.stationapi.api.template.item.TemplateItem;
 import net.modificationstation.stationapi.api.util.Namespace;
 import org.apache.logging.log4j.Logger;
 
@@ -10,4 +14,19 @@ public class Buildcraft {
 
     @Entrypoint.Logger
     public static Logger LOGGER;
+    
+    public static Item woodenGear;
+    public static Item stoneGear;
+    public static Item ironGear;
+    public static Item goldGear;
+    public static Item diamondGear;
+    
+    @EventListener
+    public void registerItems(ItemRegistryEvent event) {
+        woodenGear = new TemplateItem(NAMESPACE.id("wooden_gear")).setTranslationKey(NAMESPACE, "wooden_gear");
+        stoneGear = new TemplateItem(NAMESPACE.id("stone_gear")).setTranslationKey(NAMESPACE, "stone_gear");
+        ironGear = new TemplateItem(NAMESPACE.id("iron_gear")).setTranslationKey(NAMESPACE, "iron_gear");
+        goldGear = new TemplateItem(NAMESPACE.id("golden_gear")).setTranslationKey(NAMESPACE, "golden_gear");
+        diamondGear = new TemplateItem(NAMESPACE.id("diamond_gear")).setTranslationKey(NAMESPACE, "diamond_gear");
+    }
 }
