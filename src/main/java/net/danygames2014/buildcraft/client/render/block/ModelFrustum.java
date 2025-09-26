d7be2097dab90fb4d0eb4b438516d79fd2c6375d
Daniel Fildán <fildand@centrum.cz>
2025-09-26 09:06:13 +0200

    Fix licensing and pull it all to the main license file

--- src/main/java/net/danygames2014/buildcraft/client/render/block/ModelFrustum.java
+++ src/main/java/net/danygames2014/buildcraft/client/render/block/ModelFrustum.java
@@ -1,7 +1,3 @@
-/** Copyright (c) 2011-2015, SpaceToad and the BuildCraft Team http://www.mod-buildcraft.com
- * <p/>
- * BuildCraft is distributed under the terms of the Minecraft Mod Public License 1.0, or MMPL. Please check the contents
- * of the license located in http://www.mod-buildcraft.com/MMPL-1.0.txt */
 package net.danygames2014.buildcraft.client.render.block;
 
 import net.minecraft.client.model.ModelPart;
