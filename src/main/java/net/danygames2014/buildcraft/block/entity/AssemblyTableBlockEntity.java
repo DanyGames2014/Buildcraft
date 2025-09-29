cbda2e2d74b02376dab0fd76805f0f8082de782b
Daniel Fildán <fildand@centrum.cz>
2025-09-29 15:01:38 +0200

    [WIP] Start of lasers

--- src/main/java/net/danygames2014/buildcraft/block/entity/AssemblyTableBlockEntity.java
+++ src/main/java/net/danygames2014/buildcraft/block/entity/AssemblyTableBlockEntity.java
@@ -2,5 +2,5 @@ package net.danygames2014.buildcraft.block.entity;
 
 import net.minecraft.block.entity.BlockEntity;
 
-public class AssemblyTableBlockEntity extends BlockEntity {
+public class AssemblyTableBlockEntity extends BaseLaserTableBlockEntity {
 }
