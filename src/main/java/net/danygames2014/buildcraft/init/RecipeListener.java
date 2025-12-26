7c3136cf4457a43a138b92e44c62c5cfdd071368
Daniel Fildán <fildand@centrum.cz>
2025-12-26 06:19:12 +0100

    Adjust back chipst recipes from their testing values

--- src/main/java/net/danygames2014/buildcraft/init/RecipeListener.java
+++ src/main/java/net/danygames2014/buildcraft/init/RecipeListener.java
@@ -21,14 +21,14 @@ public class RecipeListener {
     public void registerAssemblyTableRecipes(AssemblyTableRecipeRegisterEvent event) {
         event.register(
                 NAMESPACE.id("redstone_chipset"),
-                (AssemblyTableRecipe) new AssemblyTableRecipe(1000)
+                (AssemblyTableRecipe) new AssemblyTableRecipe(10000)
                         .addInput(new ItemRecipeInput(Item.REDSTONE))
                         .addOutput(new RecipeOutput(new ItemStack(Buildcraft.redstoneChipset, 1)))
         );
         
         event.register(
                 NAMESPACE.id("iron_chipset"),
-                (AssemblyTableRecipe) new AssemblyTableRecipe(2000)
+                (AssemblyTableRecipe) new AssemblyTableRecipe(20000)
                         .addInput(new ItemRecipeInput(Item.REDSTONE))
                         .addInput(new ItemRecipeInput(Item.IRON_INGOT))
                         .addOutput(new RecipeOutput(new ItemStack(Buildcraft.redstoneIronChipset, 1)))
@@ -36,7 +36,7 @@ public class RecipeListener {
 
         event.register(
                 NAMESPACE.id("golden_chipset"),
-                (AssemblyTableRecipe) new AssemblyTableRecipe(4000)
+                (AssemblyTableRecipe) new AssemblyTableRecipe(40000)
                         .addInput(new ItemRecipeInput(Item.REDSTONE))
                         .addInput(new ItemRecipeInput(Item.GOLD_INGOT))
                         .addOutput(new RecipeOutput(new ItemStack(Buildcraft.redstoneGoldenChipset, 1)))
@@ -44,7 +44,7 @@ public class RecipeListener {
 
         event.register(
                 NAMESPACE.id("diamond_chipset"),
-                (AssemblyTableRecipe) new AssemblyTableRecipe(8000)
+                (AssemblyTableRecipe) new AssemblyTableRecipe(80000)
                         .addInput(new ItemRecipeInput(Item.REDSTONE))
                         .addInput(new ItemRecipeInput(Item.DIAMOND))
                         .addOutput(new RecipeOutput(new ItemStack(Buildcraft.redstoneDiamondChipset, 1)))
@@ -52,7 +52,7 @@ public class RecipeListener {
 
         event.register(
                 NAMESPACE.id("pulsating_chipset"),
-                (AssemblyTableRecipe) new AssemblyTableRecipe(4000)
+                (AssemblyTableRecipe) new AssemblyTableRecipe(40000)
                         .addInput(new ItemRecipeInput(Item.REDSTONE))
                         .addInput(new ItemRecipeInput(Item.DYE, 1, 5))
                         .addOutput(new RecipeOutput(new ItemStack(Buildcraft.pulsatingChipset, 2)))
