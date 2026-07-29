4e03fe21a79a549ddbd82056560673eeb0f421bf
Daniel Fildán <fildand@centrum.cz>
2026-07-29 14:16:17 +0200

    Fix crafting recipes not checking for the recipe being obstructed

--- src/main/java/net/danygames2014/buildcraft/mixin/CraftingRecipeManagerMixin.java
+++ src/main/java/net/danygames2014/buildcraft/mixin/CraftingRecipeManagerMixin.java
@@ -4,7 +4,6 @@ import net.danygames2014.buildcraft.Buildcraft;
 import net.danygames2014.buildcraft.item.FacadeItem;
 import net.minecraft.inventory.CraftingInventory;
 import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.recipe.CraftingRecipeManager;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,16 +14,24 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(CraftingRecipeManager.class)
 public class CraftingRecipeManagerMixin {
     @Inject(method = "craft", at = @At("HEAD"), cancellable = true)
-    public void craftFacade(CraftingInventory craftingInventory, CallbackInfoReturnable<ItemStack> cir){
+    public void craftFacade(CraftingInventory craftingInventory, CallbackInfoReturnable<ItemStack> cir) {
         boolean hasStructurePipe = false;
         ItemStack returnStack = null;
-        for(ItemStack stack : craftingInventory.stacks) {
+        
+        for (ItemStack stack : craftingInventory.stacks) {
             if (stack != null) {
                 if (stack.isOf(Buildcraft.cobblestoneStructurePipe.asItem())) {
+                    if (hasStructurePipe) {
+                        return;
+                    }
+                    
                     hasStructurePipe = true;
                 } else {
-                    Item var8 = stack.getItem();
-                    if (var8 instanceof BlockItem blockItem) {
+                    if (stack.getItem() instanceof BlockItem blockItem) {
+                        if (returnStack != null) {
+                            return;
+                        }
+                        
                         returnStack = FacadeItem.createStack(blockItem.getBlock(), stack.getDamage(), false);
                     }
                 }
@@ -37,16 +44,23 @@ public class CraftingRecipeManagerMixin {
     }
 
     @Inject(method = "craft", at = @At("HEAD"), cancellable = true)
-    public void convertHollowFacade(CraftingInventory craftingInventory, CallbackInfoReturnable<ItemStack> cir){
+    public void convertHollowFacade(CraftingInventory craftingInventory, CallbackInfoReturnable<ItemStack> cir) {
         ItemStack returnStack = null;
-        for(ItemStack stack : craftingInventory.stacks) {
+        
+        for (ItemStack stack : craftingInventory.stacks) {
             if (stack != null) {
                 if (stack.getItem() instanceof FacadeItem) {
-                    if(FacadeItem.isHollow(stack)){
+                    if (returnStack != null) {
+                        return;
+                    }
+                    
+                    if (FacadeItem.isHollow(stack)) {
                         returnStack = FacadeItem.createStack(FacadeItem.getBlock(stack), FacadeItem.getMeta(stack), false);
                     } else {
                         returnStack = FacadeItem.createStack(FacadeItem.getBlock(stack), FacadeItem.getMeta(stack), true);
                     }
+                } else {
+                    return;
                 }
             }
         }
